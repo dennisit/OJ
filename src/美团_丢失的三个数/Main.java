97f566c43044d7508433b77311064732702637ec
CyC2018 <1029579233@qq.com>
2018-06-23 15:15:29 +0800

    丢失的三个数

--- src/美团_丢失的三个数/Main.java
+++ src/美团_丢失的三个数/Main.java
@@ -12,14 +12,12 @@ public class Main
         for (int i = 0; i < 9997; i++) {
             bitSet.set(in.nextInt());
         }
-        int[] missing = new int[3];
-        int index = 0;
+        StringBuilder num = new StringBuilder();
         for (int i = 1; i <= 10000; i++) {
             if (!bitSet.get(i)) {
-                missing[index++] = i;
+                num.append(i);
             }
         }
-        long num = Long.valueOf(missing[0] + "" + missing[1] + "" + missing[2]);
-        System.out.println(num % 7);
+        System.out.println(Long.valueOf(num.toString()) % 7);
     }
 }
